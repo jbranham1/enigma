3aa6e44d83ea4a71dbe4aa367a3ccdb99446fc63
Jenny Branham <branhamjenny@gmail.com>
2021-01-14 14:33:37 -0600

    Create enigma class and test

---
+++ test/enigma_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
+end
