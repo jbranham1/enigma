793433bbafe3bc98e72533794b34e030de11670e
Jenny Branham <branhamjenny@gmail.com>
2021-01-15 11:46:30 -0600

    Add test for decrypt

--- test/encryptable_test.rb
+++ test/encryptable_test.rb
@@ -24,6 +24,16 @@ class EncryptableTest < Minitest::Test
     assert_equal ["k", "e", "d", "e"], result
   end
 
+  def test_encrypt_message_with_decrypt
+    result = @enigma.encrypt_message("keder ohulw", :decrypt)
+    assert_equal "hello world", result
+  end
+
+  def test_encrypt_section
+    result = @enigma.encrypt_section(["k", "e", "d", "e"], :decrypt)
+    assert_equal ["h", "e", "l", "l"], result
+  end
+
   def test_get_index_shift
     assert_equal 3, @enigma.get_index_shift(0)
   end
