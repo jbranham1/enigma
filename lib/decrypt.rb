7c9c4c4125aa4b2d061735cd5f91106b776f7d20
Jenny Branham <branhamjenny@gmail.com>
2021-01-15 16:23:53 -0600

    Changing naming on decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -5,6 +5,6 @@ file = File.open("./lib/#{ARGV[0]}", 'r')
 encrypt_file = File.open("./lib/#{ARGV[1]}", 'w')
 message = file.read.chomp
 
-encrypt  = enigma.decrypt(message, ARGV[2], ARGV[3])
-encrypt_file.write(encrypt[:encryption])
-puts "Created '#{ARGV[1]}' with the key #{encrypt[:key]} and date #{encrypt[:date]}"
+decrypt  = enigma.decrypt(message, ARGV[2], ARGV[3])
+encrypt_file.write(decrypt[:encryption])
+puts "Created '#{ARGV[1]}' with the key #{decrypt[:key]} and date #{decrypt[:date]}"
