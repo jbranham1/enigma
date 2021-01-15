d8ab6f708a22f1023324682bd02184a9fea2e4e8
Jenny Branham <branhamjenny@gmail.com>
2021-01-15 11:34:40 -0600

    Add decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,12 +4,14 @@ require_relative 'encryptable'
 class Enigma
   include Encryptable
   def encrypt(message, key, date = Date.today)
-    #todo: generate random key as optional argument
     {
       encryption: encrypt_message(message),
       key: key,
       date: convert_date(date)
     }
+  end
+
+  def decrypt
   end
 
   private
