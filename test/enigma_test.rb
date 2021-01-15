25e1b7109dd723d79ae3d391dd90cd0eee935be0
Jenny Branham <branhamjenny@gmail.com>
2021-01-15 11:34:17 -0600

    Add test for decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -30,4 +30,15 @@ class EnigmaTest < Minitest::Test
     enigma.stubs(:convert_date).returns("040895")
     assert_equal encrypt_hash, enigma.encrypt("hello world", "02715")
   end
+
+  def test_decrypt
+    enigma = Enigma.new
+    encrypt_hash = {
+      encryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal encrypt_hash, enigma.decrypt("keder ohulw", "02715","040895")
+  end
 end
