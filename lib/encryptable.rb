820ce8c1c97ab2c7f4b8947ef8acd8ef053292ba
Jenny Branham <branhamjenny@gmail.com>
2021-01-15 09:22:18 -0600

    Create encrypt messages

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -1,2 +1,40 @@
 module Encryptable
+  ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+  def encrypt_message(message)
+    split_message(message).map do |section|
+      encrypt(section)
+    end.join
+  end
+
+  def encrypt(section)
+    section.each_with_index.map do |letter, index|
+      letter == " " if letter == "_"
+      encode(letter, get_index_shift(index))
+    end
+  end
+
+  def get_index_shift(index)
+    if index == 0
+      3
+    elsif index == 1
+      27
+    elsif index == 2
+      73
+    elsif index ==3
+      20
+    end
+  end
+
+  def encode(letter, number)
+    shifted_array = ALPHABET.rotate(number)
+    index = ALPHABET.find_index(letter)
+    letter = shifted_array[index]
+  end
+
+  def split_message(message)
+    message_split = message.split("")
+    message_split.each_slice(4).to_a
+  end
 end
--- test/encryptable_test.rb
+++ test/encryptable_test.rb
@@ -13,4 +13,25 @@ class EncryptableTest < Minitest::Test
   def test_it_exists
     assert_instance_of TestClass, @enigma
   end
+
+  def test_encrypt_message
+    assert_equal "keder ohulw", @enigma.encrypt_message("hello world")
+  end
+
+  def test_encrypt_section
+    assert_equal ["k", "e", "d", "e"], @enigma.encrypt(["h", "e", "l", "l"])
+  end
+
+  def test_get_index_shift
+    assert_equal 3, @enigma.get_index_shift(0)
+  end
+
+  def test_encode
+    assert_equal "k", @enigma.encode("h", 3)
+  end
+
+  def test_split_message
+    split_array = [["h", "e", "l", "l"], ["o", "_", "w", "o"], ["r", "l", "d"]]
+    assert_equal split_array, @enigma.split_message("hello_world")
+  end
 end
